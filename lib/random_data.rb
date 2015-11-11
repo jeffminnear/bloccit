3dff709e30424c045434b3d055f0f391fbb19128
Jeff Minnear <jeffminnear@gmail.com>
2015-11-11 10:19:57 -0500

    Complete Checkpoint 30, Seeding Data

---
+++ lib/random_data.rb
@@ -0,0 +1,37 @@
+
+module RandomData
+
+  def random_paragraph
+    sentences = []
+    rand(4..6).times do
+      sentences << random_sentence
+    end
+    if rand(1..10) > 9
+      sentences.each do |sentence|
+        sentence.upcase!
+      end
+    end
+    sentences.join(" ")
+  end
+
+  def random_sentence
+    strings = []
+    rand(3..8).times do
+      strings << random_word
+    end
+
+    sentence = strings.join(" ")
+    sentence.capitalize << punctuate
+  end
+
+  def random_word
+    letters = ('a'..'z').to_a
+    letters.shuffle!
+    letters[0,rand(3..8)].join
+  end
+
+  def punctuate
+    punctuations = ['.', '!', '?']
+    punctuations[rand(0..(punctuations.length-1))]
+  end
+end
