ddfe0ee6833fb83613f9cc1237e210e4a6dd5a03
Jeff Minnear <jeffminnear@gmail.com>
2015-11-17 12:24:15 -0500

    Complete Assignment 39, Posts and Users

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,16 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_many :comments, dependent: :destroy
 
-  default_scope { order('created_at DESC') }
+  # default_scope { order('created_at DESC') }
+
+  def self.ordered_by_title
+    order('title ASC')
+  end
+
+  def self.ordered_by_reverse_created_at
+    order('created_at ASC')
+  end
+
 
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
