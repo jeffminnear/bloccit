d7790f28e0610c9172c790fbe907164e1f25318e
Jeff Minnear <jeffminnear@gmail.com>
2015-11-18 14:51:09 -0500

    Complete Checkpoint 41, Comments

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -5,7 +5,14 @@ RSpec.describe Comment, type: :model do
   let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
   let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "helloworld") }
   let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user) }
-  let(:comment) { Comment.create!(body: "Comment Body", post: post) }
+  let(:comment) { Comment.create!(body: "Comment Body", post: post, user: user) }
+
+  # Shoulda tests for relationship
+  it { should belong_to(:post) }
+  it { should belong_to(:user) }
+  # Shoulda tests for body
+  it { should validate_presence_of(:body) }
+  it { should validate_length_of(:body).is_at_least(5) }
 
   describe "attributes" do
     it "should respond to body" do
