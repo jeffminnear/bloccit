20800df395dda05e99bf58a04124d3bae461fc3e
Jeff Minnear <jeffminnear@gmail.com>
2015-11-18 11:33:09 -0500

    Update Post Authorization

--- db/seeds.rb
+++ db/seeds.rb
@@ -38,19 +38,20 @@ posts = Post.all
   )
 end
 
-# Create an admin user
-admin = User.create!(
-  name:     'Admin User',
-  email:    'admin@example.com',
-  password: 'helloworld',
-  role:     'admin'
-)
 
 # Create a member
 member = User.create!(
   name:     'Member User',
   email:    'member@example.com',
   password: 'helloworld'
+)
+
+# Create an admin user
+admin = User.create!(
+  name:     'Admin User',
+  email:    'admin@example.com',
+  password: 'helloworld',
+  role:     'admin'
 )
 
 puts "Seed finished"
