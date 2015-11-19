ce84b38e8795c3056d59a442d48b3cbc780486ea
Jeff Minnear <jeffminnear@gmail.com>
2015-11-19 09:35:47 -0500

    Add Scopes to Post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,12 @@ class Post < ActiveRecord::Base
 
   default_scope { order('created_at DESC') }
 
+  def self.order_by_title
+    reorder('title ASC')
+  end
+
+  scope :reverse_by_title, -> { order('title DESC') }
+
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
   validates :topic, presence: true
